51b9dbfc9f8d81c001431d62a546a280852dfb46
Fariz Rahman <farizrahman4u@gmail.com>
2020-08-12 17:12:06 +0530

    py27

--- wandb/sdk_py27/wandb_init.py
+++ wandb/sdk_py27/wandb_init.py
@@ -13,6 +13,7 @@ import time
 
 from six import raise_from, reraise
 import wandb
+from wandb import trigger
 from wandb.backend.backend import Backend
 from wandb.errors.error import UsageError
 from wandb.lib import console as lib_console
@@ -257,6 +258,7 @@ class _WandbInit(object):
         self._wl._early_logger_flush(logger)
 
     def init(self):
+        trigger.call("on_init", **self.config)
         s = self.settings
         config = self.config
 
