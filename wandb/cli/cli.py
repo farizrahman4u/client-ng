2ca3ff48145a213b40a5ed566f9413d2bdfd7b3c
Fariz Rahman <farizrahman4u@gmail.com>
2020-08-15 23:24:36 +0530

    cli magic command

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -833,3 +833,40 @@ def pull(run, project, entity):
                     for data in response.iter_content(chunk_size=4096):
                         f.write(data)
                         bar.update(len(data))
+
+
+@cli.command(context_settings=MONKEY_CONTEXT, help="Run any script with wandb", hidden=True)
+@click.pass_context
+@click.argument('program')
+@click.argument('args', nargs=-1)
+@display_error
+def magic(ctx, program, args):
+
+    def magic_run(cmd, globals, locals):
+        try:
+            exec(cmd, globals, locals)
+        finally:
+            pass
+
+    sys.argv[:] = args
+    sys.argv.insert(0, program)
+    sys.path.insert(0, os.path.dirname(program))
+    try:
+        with open(program, 'rb') as fp:
+            code = compile(fp.read(), program, 'exec')
+    except IOError:
+        click.echo(click.style("Could not launch program: %s" % program, fg="red"))
+        sys.exit(1)
+    globs = {
+            '__file__': program,
+            '__name__': '__main__',
+            '__package__': None,
+            'wandb_magic_install': magic_install,
+        }
+    prep = '''
+import __main__
+__main__.__file__ = "%s"
+wandb_magic_install()
+''' % program
+    magic_run(prep, globs, None)
+    magic_run(code, globs, None)
